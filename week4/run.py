adfac1c2f39ab0b3f64027fb66b48c27928eb10b
Renee <evilrenee@gmail.com>
2018-08-07 11:58:49 +1200

    Built order page

---
+++ week4/run.py
@@ -0,0 +1,57 @@
+from flask import Flask, render_template, request
+import sqlite3
+
+app = Flask(__name__)
+
+MENUDB = 'menu.db'
+
+def fetchMenu(con):
+    burgers = []
+    cur = con.execute('SELECT burger, price FROM burgers')
+    for row in cur:
+        burgers.append(list(row))
+
+    drinks = []
+    cur = con.execute('SELECT drinks, price FROM drinks')
+    for row in cur:
+        drinks.append(list(row))
+
+    sides = []
+    cur = con.execute('SELECT sides, price FROM sides')
+    for row in cur:
+        sides.append(list(row))
+
+    return {'burgers':burgers, 'drinks': drinks, "sides": sides}
+
+@app.route('/')
+def index():
+    con = sqlite3.connect(MENUDB)
+    menu = fetchMenu(con)
+    con.close()
+
+    return render_template('index.html', disclaimer='may contain traces of nuts', burgers=menu['burgers'], drinks=menu['drinks'], sides=menu['sides'])
+
+@app.route('/order')
+def order():
+    con = sqlite3.connect(MENUDB)
+    menu = fetchMenu(con)
+    con.close()
+    return render_template('order.html', burgers=menu['burgers'], drinks=menu['drinks'], sides=menu['sides'])
+
+@app.route('/confirm', methods=['POST'])
+def confirm():
+    details = {}
+    items = {}
+
+    for input in request.form:
+        if input == 'name' or input == 'address':
+            details[input] = request.form[input]
+
+        elif request.form[input] and request.form[input] != '0':
+            items[input] = request.form[input]
+
+    con = sqlite3.connect(MENUDB)
+    cur = con.execute('INSERT INTO orders(name, address, items) VALUES(?, ?, ?)', ( details['name'], details['address'], str(items)) )
+    con.commit()
+
+    return render_template('confirm.html', details=details, items=items)
